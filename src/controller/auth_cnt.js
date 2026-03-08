12cb8e8ee9d38de7e010e00e670e31885d4cb8dd
Satya <satyabratapadhiary6@gmail.com>
2026-03-08 11:10:23 +0530

    feat: user login

--- src/controller/auth_cnt.js
+++ src/controller/auth_cnt.js
@@ -1,46 +1,91 @@
 import express from 'express'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 import userinputvalidate from '../middlewares/userinputvalidate.js'
 import { prisma } from '../utils/prisma.js'
 const authrouter = express.Router()
 
-authrouter.post('/signup',userinputvalidate,async(req,res,next)=>{
+authrouter.post('/signup', userinputvalidate, async (req, res, next) => {
   try {
-    const {name,email,password,role} = req.body
-    if(!name || !email || !password || !role){
+    const { name, email, password, role } = req.body
+    if (!name || !email || !password || !role) {
       return res.status(400).send({
-        success:false,
-        error:"Invalid request schema"
+        success: false,
+        error: "Invalid request schema"
       })
     }
-    const encrypt =  await bcrypt.hash(password,10)
+    const encrypt = await bcrypt.hash(password, 10)
     const newuser = await prisma.user.create({
-      data:{
-        name:name,
-        email:email,
-        passwordHash:encrypt,
-        role:role.toUpperCase()
+      data: {
+        name: name,
+        email: email,
+        passwordHash: encrypt,
+        role: role.toUpperCase()
       }
     })
     res.status(201).send({
-      success:true,
-      data:{
+      success: true,
+      data: {
         id: newuser.id,
-        name:newuser.name,
-        email:newuser.email,
-        role:newuser.role
+        name: newuser.name,
+        email: newuser.email,
+        role: newuser.role
       }
-  
+
     })
   } catch (error) {
-    if(error.name == "PrismaClientKnownRequestError"){
+    if (error.name == "PrismaClientKnownRequestError") {
       return res.status(400).send({
-        success:false,
-        error:"Email already exists"
+        success: false,
+        error: "Email already exists"
       })
     }
     next(error)
   }
 })
 
-export {authrouter}
+authrouter.post('/login', userinputvalidate, async (req, res, next) => {
+  try {
+    const { email, password } = req.body
+    if (!email || !password) {
+      return res.status(400).send({
+        success: false,
+        error: "Invalid request schema"
+      })
+    }
+    
+    const user = await prisma.user.findUnique({
+      where:{
+        email:email
+      }
+    })
+    if(!user){
+      return res.status(404).send({
+        success:false,
+        error:"Wrong credentials"
+      })
+    }
+    const passverify = await bcrypt.compare(password,user.passwordHash)
+    if(!passverify){
+      return res.status(404).send({
+        success:false,
+        error:"Wrong credentials"
+      })
+    }
+    const payload = {
+      userId:user.id,
+      role:user.role
+    }
+    const token = jwt.sign(payload,process.env.SECRET)
+    res.send({
+      success:true,
+      data:{
+        token:token
+      }
+    })
+  } catch (error) {
+    next(error)
+  }
+})
+
+export { authrouter }
